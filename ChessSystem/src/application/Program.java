4321710ebd1cd89a8475947a8418fdf227286744
vininiceto <viniciusniceto@gmail.com>
2023-09-26 14:49:30 -0300

    Criação da classe Position

--- ChessSystem/src/application/Program.java
+++ ChessSystem/src/application/Program.java
@@ -1,13 +1,15 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
 		
+		Position position = new Position(3, 20);
 		
-		
-		
+		System.out.println(position);
 		
 		
 		
