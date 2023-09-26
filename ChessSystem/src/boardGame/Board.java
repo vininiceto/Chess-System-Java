2b5dfeba7d155340f6ab416ac16da6974b49f81b
vininiceto <viniciusniceto@gmail.com>
2023-09-26 15:05:01 -0300

    Adicionando classe Board e Piece

---
+++ ChessSystem/src/boardGame/Board.java
@@ -0,0 +1,16 @@
+package boardGame;
+
+public class Board {
+	
+	private Integer rows;
+	private Integer columns;
+	private Piece[][] pieces;
+	public Board(Integer rows, Integer columns) {
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece[rows][columns];
+	}
+
+	
+	
+}
